52518746ce7105e3fe3e70f4eace6577d72f66ee
COINMAC Inc <coinmacsms@gmail.com>
2019-08-29 15:14:15 +0100

    My Courses Page 4

--- resources/views/mycourses.blade.php
+++ resources/views/mycourses.blade.php
@@ -21,36 +21,29 @@
                         </div>
                         <div class="col-md-9">
                             @foreach ($mycourses as $course)
-                            
-                            
                                 <div class="col-md-6">
-                                    <div class="card bg-info">
-                                        <div class="card-header">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading">
                                             <div class="row">
                                                 <div class="col-sm-6">
                                                         {{$course->payment}}
                                                 </div>
-                                            </div>
-
-                                            <div class="row">
+                                            
                                                 <div class="col-sm-6">
                                                         Status: {{$course->approval}}
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="card-body">
-                                            <h5 class="card-title">{{$course->coursename}}</h5>
-                                            <p class="card-text">Date: {{$course->dates}}</p>
-                                            <p class="card-text">Venue: {{$course->venue}}</p>
+                                        <div class="panel-body">
+                                            <h5 class="panel-title">{{$course->coursename}}</h5>
+                                            <p class="panel-text">Date: {{$course->dates}}</p>
+                                            <p class="panel-text">Venue: {{$course->venue}}</p>
                                         </div>
-                                        <div class="card-footer">
+                                        <div class="panel-footer">
                                                 <div class="row">
                                                     <div class="col-sm-4" style="font-weight: bold; color: red">
                                                             Fee: {{$course->amount}}
                                                     </div>
-                                                </div>
-            
-                                                <div class="row">
                                                     <div class="col-sm-8">
                                                             <div class="btn-group" role="group" aria-label="Button group">
                                                                 <a href="/deletecourse/{{$course->id}}" class="btn-group btn-xs btn-danger" onclick="return confirm('Are you sure you want to delete this course from your account?')">Delete</a>
@@ -61,7 +54,7 @@
                                                 </div>
                                         </div>
                                     </div>
-                                </div>                                
+                                </div>                                                                  
                             @endforeach
                             
                         </div>
