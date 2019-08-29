a4a85807eb55f7c51656ba96233363434b9f24e5
COINMAC Inc <coinmacsms@gmail.com>
2019-08-29 11:03:47 +0100

    Login

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -4,9 +4,9 @@
 <hr>
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6 col-md-offset-3">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header h1">{{ __('Verify Your Email Address') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
@@ -14,9 +14,15 @@
                             {{ __('A fresh verification link has been sent to your email address.') }}
                         </div>
                     @endif
+                        <div class="alert alert-success" role="alert">
+                            
+                            {{ __('Before proceeding, please check your email for a verification link.') }}
+                            {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                        </div>
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                        <div class="float-right">
+                            Already Verified? <a href="/login" class="btn btn-primary float-right">Click Here to Login</a>
+                        </div>
                 </div>
             </div>
         </div>
