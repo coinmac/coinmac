b8bc63d4c9ccf21f3b8022733ee5debb47f02af7
COINMAC Inc <coinmacsms@gmail.com>
2019-08-29 11:53:42 +0100

    Master edited 5

--- routes/web.php
+++ routes/web.php
@@ -45,7 +45,7 @@ Route::get('/coursegroup/{courseid}/{type}/{coursename}','CoursecatrgoriesContro
 
 Route::get('/sc/{subjectid}','CoursecatrgoriesController@course');
 
-Route::get('/registercourse/{subjectid}/{amount}','CoursecatrgoriesController@courseregister');
+Route::get('/registercourse/{subjectid}/{amount}','CoursecatrgoriesController@registercourseregister');
 
 Route::put('/paycourse/{id}', 'CoursecatrgoriesController@paycourse')->name('paycourse');
 
