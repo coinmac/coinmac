fc5dd4e9ff589be155da13aea00bf319a8c63e20
COINMAC Inc <coinmacsms@gmail.com>
2019-09-09 14:40:06 +0100

    Subscriptions 1

--- app/subjectlists.php
+++ app/subjectlists.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class subjectlists extends Model
 {
-    //
+    protected $guarded = [];
 }
--- app/subscriptions.php
+++ app/subscriptions.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class subscriptions extends Model
 {
-    //
+    protected $guarded = [];
 }
