e76f21a6df49220a8134f85abf8f1eb6ebed22b8
COINMAC Inc <coinmacsms@gmail.com>
2019-09-09 11:23:34 +0100

    New Updates 3

--- resources/views/ccmail.blade.php
+++ resources/views/ccmail.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-{!! $type = \App\use App\coursecatrgories::where('courseid','=',$cc->coursecatid)->first()->category; !!}
+{!! $type = \App\coursecatrgories::where('courseid','=',$cc->coursecatid)->first()->category; !!}
 <div style="background-image:url('https://coinmac.org/images/course/{{$cc->subjectid}}/{{$cc->image}}'); background-size: cover; background-color:darkblue; width: 100%; min-heigh: 100px; height: auto; padding: 20px; text-align: center;">
     <h2 style="color: white; text-shadow: 2px 2px #000;">{{$cc->subjectname}}</h2>
     <h4>Category: {{$cc->coursecategory}} | {{$type}} </h4>
