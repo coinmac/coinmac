e59932eafeda26aab1366e492437a1f366ebea46
COINMAC Inc <coinmacsms@gmail.com>
2019-09-09 16:16:51 +0100

    Slie

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,78 @@
             <!-- SLIDE 1 -->
             <li data-index="rs-3049" data-transition="slideremovedown" data-slotamount="7" data-hideafterloop="0" data-hideslideonmobile="off"  data-easein="Power4.easeInOut" data-easeout="Power4.easeInOut" data-masterspeed="2000"    data-rotate="0"  data-saveperformance="off"  data-title="Ken Burns" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">                        
 
+                    <!-- MAIN IMAGE -->
+                    <img src="images/slides/slide2.jpg"  alt=""  data-bgposition="center center" data-kenburns="off" data-duration="30000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" data-bgparallax="10" class="rev-slidebg" data-no-retina>
+                    <!-- LAYERS -->
+    
+                    <!-- LAYER NR. 12 -->
+                    <div class="tp-caption title-slide" 
+                        id="slide-3049-layer-1" 
+                        data-x="['left','left','left','left']" data-hoffset="['0','0','0','0']" 
+                        data-y="['middle','middle','middle','middle']" data-voffset="['-77','-77','-77','-77']" 
+                        data-fontsize="['72','72','45','35']"
+                        data-lineheight="['60','60','45','35']"
+                        data-fontweight="['700','700','700','700']"
+                        data-width="none"
+                        data-height="none"
+                        data-whitespace="nowrap"
+             
+                        data-type="text" 
+                        data-responsive_offset="on"                             
+    
+                        data-frames='[{"delay":1000,"speed":2000,"frame":"0","from":"y:-50px;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+    
+                        data-textAlign="['left','left','left','left']"
+                        data-paddingtop="[18,18,18,18]"
+                        data-paddingright="[0,0,0,0]"
+                        data-paddingbottom="[0,0,0,0"
+                        data-paddingleft="[0,0,0,0]"
+    
+                        style="z-index: 16; white-space: nowrap;text-transform:left;"><span>COINMAC Inc</span><br>THE STAR SOLUTION FOR PRODUCTIVITY
+                    </div>
+    
+                    <!-- LAYER NR. 13 -->
+                    <div class="tp-caption sub-title" 
+                        id="slide-3049-layer-4" 
+                        data-x="['left','left','left','left']" data-hoffset="['0','0','0','0']" 
+                        data-y="['middle','middle','middle','middle']" data-voffset="['55','55','0','0']"
+                        data-fontsize="['16',16','14','14']" 
+                        data-fontweight="['300','300','300','300']"
+                        data-width="660"
+                        data-height="none"
+                        data-lineheight="['16','16','16','16']"
+                        data-whitespace="nowrap"
+             
+                        data-type="text" 
+                        data-responsive_offset="on" 
+    
+                        data-frames='[{"from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","speed":2000,"to":"o:1;","delay":1500,"ease":"Power4.easeInOut"},{"delay":"wait","speed":1000,"to":"y:[100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"Power2.easeInOut"}]'
+                        data-textAlign="['left','left','left','left']"
+                        data-paddingtop="[0,0,0,0]"
+                        data-paddingright="[0,0,0,0]"
+                        data-paddingbottom="[0,0,0,0]"
+                        data-paddingleft="[0,0,0,0]"
+    
+                        style="z-index: 17; white-space: nowrap;text-transform:left;">Our courses cuts accross all the fundamental and relavant professional fields</div>
+    
+                    <a href="#" target="_self" class="tp-caption flat-button-slider bg-orange"         
+    
+                    data-frames='[{"from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","speed":2000,"to":"o:1;","delay":2000,"ease":"Power4.easeInOut"},{"delay":"wait","speed":1000,"to":"y:[100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"Power2.easeInOut"}]'
+                 
+                    data-x="['left','left','left','left']" data-hoffset="['0','0','0','0']" 
+                    data-y="['middle','middle','middle','middle']" data-voffset="['188','188','188','50']" 
+                    data-width="['auto']"
+                    data-fontsize="['13',13','11','11']" 
+                    data-fontweight="['500','500','500','500']"
+                    data-lineheight="['46','46','38','38']"
+                    data-paddingright="['23','23','23','23']"
+                    data-paddingleft="['26','26','26','26']"
+                    data-height="['auto']">START A COURSE
+                    </a><!-- END LAYER LINK -->
+                </li>
+
+            <li data-index="rs-3049" data-transition="slideremovedown" data-slotamount="7" data-hideafterloop="0" data-hideslideonmobile="off"  data-easein="Power4.easeInOut" data-easeout="Power4.easeInOut" data-masterspeed="2000"    data-rotate="0"  data-saveperformance="off"  data-title="Ken Burns" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">                        
+
                 <!-- MAIN IMAGE -->
                 <img src="images/slides/slides1.jpg"  alt=""  data-bgposition="center center" data-kenburns="off" data-duration="30000" data-ease="Linear.easeNone" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" data-bgparallax="10" class="rev-slidebg" data-no-retina>
                 <!-- LAYERS -->
