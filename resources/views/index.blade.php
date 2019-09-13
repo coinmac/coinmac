ecb2db62eb101de5a923b937779e3afa5e2a9d6b
COINMAC Inc <coinmacsms@gmail.com>
2019-09-13 15:43:40 +0100

    New Changes 8

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -344,7 +344,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            ,a href="#"><img src="images/diploma.jpg" alt="DIPLOMA BANNER"></a>
+            <a href="#"><img src="images/diploma.jpg" alt="DIPLOMA BANNER"></a>
         </div>
         <div class="col-md-12">
 
