e1d4fd85f12b2a855afda7ad3584bc30b0f912f8
COINMAC Inc <coinmacsms@gmail.com>
2019-10-04 11:55:25 +0100

    Diploma Registrations

--- resources/views/amaster.blade.php
+++ resources/views/amaster.blade.php
@@ -143,6 +143,7 @@
                                                     <li><a href="/edit_courses">Add / Send / Edit Courses</a></li>                                                
                                                     <li><a href="/edit_ccategories">Add / Send / Edit Course Categories</a></li>
                                                     <li><a href="/contents">Publish / Edit Site Contents</a></li>
+                                                    <li><a href="/diplomaregs">Diploma Registrations</a></li>
                                                     <li><a href="/logout">Logout</a></li>
                                                 </ul>
                                             </li>
