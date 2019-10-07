7799dfa89bd8585b33971e4687d4e077d63d258b
COINMAC Inc <coinmacsms@gmail.com>
2019-10-07 17:36:51 +0100

    coinmac sss

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -378,7 +378,7 @@
                     @if($cc->coursename!='All')
                         <div class="col-md-3 col-sm-6" style="padding: 5px; font-weight: bold; background-color: #3387AF; text-align: center; height: 70px; border: 5px white solid;">   
                                 @if($cc->image!='catcover.jpg')
-                                    <img src="/images/content/{{$cc->image}}" width="100%" height="auto">
+                                    <img src="/images/content/{{$cc->image}}" width="100%" height="auto"><br>
                                 @endif
                             <a href="/coursegroup/{{$cc->courseid}}/{{$cc->category}}/{{$cc->coursename}}" style="color: white !important;">{{ucwords(strtolower($cc->coursename))}}</a>
                         </div>   
